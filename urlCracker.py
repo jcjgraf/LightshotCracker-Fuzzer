5d947f0b1df58f09f536e85ff8073ae7ebada34a
Jean-Claude Graf <jeanggi90@gmail.com>
2017-09-01 10:19:54 +0200

    Successfully get the url of the image

--- urlCracker.py
+++ urlCracker.py
@@ -1,1 +1,41 @@
-#!/bin/bash/python 
+#!/usr/bin/env python3
+
+#############
+# LIBRARIES #
+#############
+
+import requests
+from lxml import html
+
+
+############
+# FUNCTION #
+############
+
+# def setupBrowser
+
+def getImage(url):
+	with requests.Session() as c:
+		sourceCode = c.get(url).content
+
+	tree = html.fromstring(sourceCode)
+
+	img = tree.xpath('//img/@src')[1]
+
+	print(img)
+
+	# witeToFile(sourceCode)
+
+def witeToFile(toSave):
+	with open('test.html', 'w') as file:
+		file.write(str(toSave))
+
+
+########
+# MAIN #
+########
+
+if __name__ == '__main__':
+	getImage("http://prntscr.com/gfpo6w")
+
+
